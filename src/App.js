0b4c0ed7b60d161beca4765ba7ba0241a9fe6e30
Ghanshyam <gk.paircode@gmail.com>
2023-06-17 05:51:18 +0530

    removed all components from App.js so new one can be added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import "./styles.css";
-import EmpTable from './components/EmpTable';
-// import MemoisedDemo from './components/MemoisedDemo';
-// import VowelsCount from './components/VowelsCount';
 
 export default function App() {
 
   return (
     <div className="App">
-      <h1>Date : June 16, 2023 </h1>
-      {/* <VowelsCount /> */}
-      {/* <MemoisedDemo /> */}
-      <EmpTable />
+      <h1>Date : June 17, 2023 </h1>
     </div>
   );
 }
